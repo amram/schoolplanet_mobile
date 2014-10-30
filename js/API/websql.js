1214d37b52f69ac10cc6d54c13267577abcbfa08
Amram López Ochoa <amram_x@hotmail.com>
2014-10-30 12:58:03 -0600

    2
    2

---
+++ js/API/websql.js
@@ -0,0 +1,93 @@
+var db = {
+    crearDB: function(){
+        var obj = window.openDatabase("hotel", "1.0", "Hotel Demo", 200000);
+        return obj;
+    },
+    //--------------------PENDIENTES------------------
+    agregarPendientes: function(th,ha,pr,di){
+        db.th = th;
+        db.ha = ha;
+        db.pr = pr;
+        db.di = di;
+        db.crearDB().transaction(db.tablaPendientes,db.error,db.exitoPendientes);
+    },
+    tablaPendientes: function(tx){
+        tx.executeSql("CREATE TABLE IF NOT EXISTS pendientes (id unique, th, ha, pr, di)");
+        tx.executeSql("INSERT INTO pendientes (th, ha, pr, di) VALUES ('" + db.th + "', '" + db.ha + "', '" + db.pr + "', '" + db.di + "')");
+    },
+    exitoPendientes: function(){
+        $.mobile.loading( 'hide' );
+        navigator.notification.alert('Reserva en espera de conexión',null,'Guardado','Aceptar');
+    },
+    leerPendientes: function(){
+        //alert(1);
+        db.crearDB().transaction(db.selectPendientes,db.error,null);
+    },
+    selectPendientes: function(tx){
+        //alert(2);
+        tx.executeSql("SELECT * FROM pendientes",[],db.resultadosPendientes,db.error);
+    },
+    resultadosPendientes: function(tx,res){
+        //alert(3);
+        var cant = res.rows.length;
+        if(cant>0){
+            for(var i = 0;i < cant;i++){
+                //alert(i);
+                var th = res.rows.item(i).th;
+                var ha = res.rows.item(i).ha;
+                var pr = res.rows.item(i).pr;
+                var di = res.rows.item(i).di;
+                
+                fn.enviarReserva(th,ha,pr,di);
+                tx.executeSql("DELETE FROM pendientes WHERE id='" + res.rows.item(i).id + "'");
+            }
+        }
+    },
+    //-------------------HISTORIAL---------------------
+    agregarHistorial: function(th,ha,pr,di){
+        db.th = th;
+        db.ha = ha;
+        db.pr = pr;
+        db.di = di;
+        db.crearDB().transaction(db.tablaHistorial,db.error,db.exitoHistorial);
+    },
+    tablaHistorial: function(tx){
+        tx.executeSql("CREATE TABLE IF NOT EXISTS historial (id unique, th, ha, pr, di)");
+        tx.executeSql("INSERT INTO historial (th, ha, pr, di) VALUES ('" + db.th + "', '" + db.ha + "', '" + db.pr + "', '" + db.di + "')");
+    },
+    exitoHistorial: function(){
+        $.mobile.loading( 'hide' );
+        navigator.notification.alert('Se ha registrado su reserva',null,'Reserva Exitosa','Aceptar');
+    },
+    error: function(err){
+        $.mobile.loading( 'hide' );
+        alert('Error: '+err.code);
+    },
+	
+	 leerHistorial: function(){   
+        db.crearDB().transaction(db.selectHistorial,db.error,null);
+    },
+    selectHistorial: function(tx){
+ 
+        tx.executeSql("SELECT * FROM historial",[],db.resultadosHistorial,db.error);
+    },
+    resultadosHistorial: function(tx,res){
+		
+        var cant = res.rows.length;
+        if(cant>0){
+			var x="";
+            for(var i = 0;i < cant;i++){
+           
+                var th = res.rows.item(i).th;
+                var ha = res.rows.item(i).ha;
+                var pr = res.rows.item(i).pr;
+                var di = res.rows.item(i).di;
+                
+				x+="<tr><td>"+th+"</td><td>"+ha+"</td><td>"+pr+"</td><td>"+di+"</td></tr>";
+				
+			}
+			$('#tbody').html(x);
+				
+		}
+	}
+}
--- js/actions.js
+++ js/actions.js
@@ -13,6 +13,13 @@ var fn = {
         $('#showStorage').tap(function(){
             alert(fn.storage.getItem('registro'));
         });
+        
+        $('#nr1 ul[data-role=listview] li').tap(fn.selectTH);
+        $('#nr1 div[data-role=navbar] ul li:eq(1)').tap(fn.nrNext);
+        $('#nr2 div[data-role=navbar] ul li:eq(1)').tap(fn.reservar);
+        
+        document.addEventListener("online",fn.sincronizarReserva,false);
+		$('#verHistorial').tap(db.leerHistorial);
     },
     registro: function(){
         var nombre = $('#regName').val();
@@ -38,12 +45,60 @@ var fn = {
             }
         });
     },
+    enviarReserva: function(th, ha, pr, di){
+        $.mobile.loading( 'show' );
+        $.ajax({
+            type: "POST",
+            url: "http://carlos.igitsoft.com/apps/test.php",
+            data: {tipo:th,habitaciones:ha,personas:pr,dias:di}
+        }).done(function(respuesta){
+            if( respuesta == '1' ){
+                db.agregarHistorial(th,ha,pr,di);
+            }
+        });
+    },
     storage: window.localStorage,
     estaRegistrado: function(){
         if(fn.storage.getItem('registro') == 1)
             return true;
         else
             return false;
+    },
+    selectTH: function(){
+        if($(this).index()>0){
+            $('#nr1 ul[data-role=listview] li a').css('background-color','');
+            $(this).find('a').css('background-color','#a9a9a9');
+            $('#nr1').attr('th',$(this).index());
+        }
+    },
+    nrNext: function(){
+        if($('#nr1').attr('th') != undefined && $('#nr1').attr('th') != ''){
+            window.location.href = "#nr2";
+        }else{
+            alert('Es necesario seleccionar un tipo de habitación');
+        }
+    },
+    reservar: function(){
+        var th = $('#nr1').attr('th');
+        var ha = $('#nrHab').val();
+        var pr = $('#nrPer').val();
+        var di = $('#nrDia').val();
+        
+        if(th != '' && ha != '' && pr != '' && di != ''){
+            if(connection.estaConectado()){
+                //Enviar Reserva a servidor
+                fn.enviarReserva(th,ha,pr,di);
+            }else{
+                //Guardar los datos hasta conexión
+                $.mobile.loading( 'show' );
+                db.agregarPendientes(th,ha,pr,di);
+            }
+        }else{
+            alert('Todos los campos son requeridos');
+        }
+    },
+    sincronizarReserva: function(){
+        db.leerPendientes();
     }
 };
 $(fn.init);
